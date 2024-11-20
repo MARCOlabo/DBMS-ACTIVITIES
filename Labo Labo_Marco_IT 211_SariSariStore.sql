ee50362ca7e0f9a2516cdb9b073f54341ddd9550
MARCOlabo <labolabomarco@gmail.com>
2024-11-20 16:22:35 +0800

    Add files via upload
    DMS Activity - Sari Sari Store

---
+++ Labo Labo_Marco_IT 211_SariSariStore.sql
@@ -0,0 +1,37 @@
+-- create table store(
+--   Product_no int primary key auto_increment,
+--   Product_name varchar(50) not null,
+--   product_calories int,
+--   Quantity int,
+--   Price int not null
+--  
+--  )
+
+
+-- insert into store( Product_name, product_calories, Quantity, Price) values
+-- ('pampers', 0, 15, 10),
+-- ('sprite', 12, 2, 12),
+-- ('coke', 12, 3, 12),
+-- ('royal', 12, 4, 12),
+-- ('mirranda', 12, 4, 12),
+-- ('mountain dew', 12, 4, 16),
+-- ('7-up', 16, 4, 15),
+-- ('coca-cola', 18, 4, 20),
+-- ('cranklings', 12, 8, 8),
+-- ('botbawang', 2, 9, 9),
+-- ('redhorse', 20, 10, 107),
+-- ('empilight', 12, 10, 58),
+-- ('candy', 0, 20, 3),
+-- ('mancher', 9, 6, 8),
+-- ('toblerone', 16, 5, 18),
+-- ('stick-o', 5, 5, 14),
+-- ('magnum', 16, 5, 20),
+-- ('alaska', 8, 2, 20),
+-- ('ber brand', 10, 2, 20),
+-- ('noor cubes', 3, 2, 10),
+-- ('chicken', 7, 2, 50),
+-- ('lemon juice', 10, 2, 12),
+-- ('napkin', 0, 2, 18),
+-- ('tissue', 0, 2, 20)
+
+select * from store;
